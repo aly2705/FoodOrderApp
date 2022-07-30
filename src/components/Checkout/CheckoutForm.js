9d46f6b303f58b931d68fa8297daa6f49811f9b2
aly2705 <alexandraaly.simion@gmail.com>
2022-07-30 23:17:58 +0300

    styled the checkout form

--- src/components/Checkout/CheckoutForm.js
+++ src/components/Checkout/CheckoutForm.js
@@ -10,16 +10,42 @@ const CheckoutForm = props => {
     event.preventDefault();
     console.log('Submitted');
     console.log(cartContext.items, cartContext.totalAmount);
+
+    props.onClose();
+    cartContext.clearCart();
   };
   return (
     <Modal onClose={props.onClose}>
-      <form className={classes.checkout__form} onSubmit={submitOrderHandler}>
-        <div>
-          <button type="submit">Send order</button>
+      <form className={classes.form} onSubmit={submitOrderHandler}>
+        <div className={classes.form__group}>
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" id="first-name" />
+        </div>
+        <div className={classes.form__group}>
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" id="last-name" />
+        </div>
+        <div className={classes.form__group}>
+          <label htmlFor="phone-number">Phone Number</label>
+          <input type="tel" id="phone-number" />
+        </div>
+        <div className={classes.form__group}>
+          <label htmlFor="address">Address</label>
+          <input type="text" id="address" />
+        </div>
+        <div className={classes.form__buttons}>
+          <button
+            type="button"
+            className={classes['button-alt']}
+            onClick={props.onClose}
+          >
+            Close
+          </button>
+          <button type="submit" className={classes.button}>
+            Send order
+          </button>
         </div>
       </form>
-      <p>Checking out... </p>
-      <p>Your order has a price of ${cartContext.totalAmount.toFixed(2)}</p>
     </Modal>
   );
 };
